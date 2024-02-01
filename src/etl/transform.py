d51f068ee8c7158b33ab2698d50129602e2cff52
lucasjotap <leiathemosthot@gmail.com>
2024-02-01 20:40:29 -0300

    added docs

--- src/etl/transform.py
+++ src/etl/transform.py
@@ -8,38 +8,45 @@ from schema import custom_schema
 
 class Transform(object):
 
-	def __init__(self):
-		self.spark: SparkSession = SparkHandler.create_session()
-		self.processed_data_path: str = "/home/lucas/Desktop/Python/large-scale-data-processing/data/raw/*.parquet"
-		self.write_to_processed_layer_data_path: str = '/home/lucas/Desktop/Python/large-scale-data-processing/data/output/'
+    def __init__(self):
+        # Inicializa a sessão do Spark
+        self.spark: SparkSession = SparkSession.builder.appName("Transformação de Dados").getOrCreate()
+        
+        # Define os caminhos dos dados de entrada e saída
+        self.processed_data_path: str = "/home/lucas/Desktop/Python/large-scale-data-processing/data/raw/*.parquet"
+        self.write_to_processed_layer_data_path: str = '/home/lucas/Desktop/Python/large-scale-data-processing/data/output/'
 
-	def data_to_transform(self):
-		"""
-		"""
-		return self.spark.read.schema(custom_schema).parquet(self.processed_data_path)
-		
-	def transfomation_action(self):
-		"""
-		"""
-		df = self.data_to_transform()
+    def data_to_transform(self):
+        """
+        Carrega os dados de entrada usando o esquema customizado.
+        """
+        return self.spark.read.schema(custom_schema).parquet(self.processed_data_path)
+        
+    def transfomation_action(self):
+        """
+        Realiza a transformação necessária nos dados.
+        """
+        df = self.data_to_transform()
 
-		# df = df.withColumn("VendorID", col("VendorID").cast(LongType()))
+        # Comente ou descomente a linha abaixo conforme a necessidade de conversão do tipo VendorID
+        # df = df.withColumn("VendorID", col("VendorID").cast(LongType()))
 
-		result_df = (
-		    df
-		    .groupBy('VendorID')
-		    .agg(sum(col('fare_amount')
-		    .cast('double'))
-		    .alias('total_fare'))
-		    )
-		return result_df
+        # Realiza a agregação por VendorID, somando a coluna fare_amount convertida para double
+        result_df = (
+            df
+            .groupBy('VendorID')
+            .agg(sum(col('fare_amount').cast('double')).alias('total_fare'))
+        )
+        return result_df
 
-	def output_data(self):
-		"""
-		"""
-		df = self.transfomation_action()
-		df.write.mode('overwrite').parquet(self.write_to_processed_layer_data_path)
+    def output_data(self):
+        """
+        Escreve os dados transformados na camada de saída.
+        """
+        df = self.transfomation_action()
+        df.write.mode('overwrite').parquet(self.write_to_processed_layer_data_path)
 
 if __name__ == "__main__":
-	transform: Transform = Transform()
-	transform.output_data()
+    # Instancia a classe de transformação e executa a saída de dados
+    transform: Transform = Transform()
+    transform.output_data()
